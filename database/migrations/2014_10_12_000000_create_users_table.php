e443730bc391787de8bdaf2ab74bbe55e7214a41
Syaichul <syaichulahmad@gmail.com>
2024-08-15 15:09:44 +0700

    change join and date

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('user_id');
             $table->string('email')->unique();
-            $table->string('join_date')->unique();
-            $table->string('last_login')->unique();
+            $table->string('join_date')->nullable();
+            $table->string('last_login')->nullable();
             $table->string('phone_number')->nullable();
             $table->string('status')->nullable();
             $table->string('role_name')->nullable();
