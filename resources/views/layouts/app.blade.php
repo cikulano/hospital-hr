7ab946cc32f6517ffe4a5400305ed2376458337e
Syaichul <syaichulahmad@gmail.com>
2024-08-19 14:31:48 +0700

    vercel v5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,55 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <meta name="description" content="soengsouy - Bootstrap Admin Template">
-        <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
-        <meta name="author" content="soengsouy">
-        <meta name="robots" content="noindex, nofollow">
-        <title>Login HRMS</title>
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-        <!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-        <!-- Lineawesome CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
-        <!-- Select2 CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
-        <!-- Datetimepicker CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}">
-        <!-- Main CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-        {{-- message toastr --}}
-        <link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
-        <script src="{{ asset('assets/js/toastr_jquery.min.js') }}"></script>
-        <script src="{{ asset('assets/js/toastr.min.js') }}"></script>
-    </head>
-    <body class="account-page error-page">
-        <style>    
-            .invalid-feedback{
-                font-size: 14px;
-            }
-        </style>
-        <!-- Main Wrapper -->
-        @yield('content')
-        <!-- /Main Wrapper -->
-        <!-- jQuery -->
-        <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
-        <!-- Bootstrap Core JS -->
-        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-        <!-- Slimscroll JS -->
-        <script src="{{ asset('assets/js/jquery.slimscroll.min.js') }}"></script>
-        <!-- Select2 JS -->
-        <script src="{{ asset('assets/js/select2.min.js') }}"></script>
-        <!-- Datetimepicker JS -->
-        <script src="{{ asset('assets/js/moment.min.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
-        <!-- Custom JS -->
-        <script src="{{ asset('assets/js/app.js') }}"></script>
-        @yield('script')
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="soengsouy - Bootstrap Admin Template">
+    <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
+    <meta name="author" content="soengsouy">
+    <meta name="robots" content="noindex, nofollow">
+    <title>Login HRMS</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <!-- Lineawesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
+    <!-- Datetimepicker CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    {{-- message toastr --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/toastr.min.css') }}">
+    <script src="{{ asset('assets/js/toastr_jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/toastr.min.js') }}"></script>
+</head>
+<body class="account-page error-page">
+    <style>    
+        .invalid-feedback {
+            font-size: 14px;
+        }
+    </style>
+    <!-- Main Wrapper -->
+    @yield('content')
+    <!-- /Main Wrapper -->
+    <!-- jQuery -->
+    <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
+    <!-- Bootstrap Core JS -->
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <!-- Slimscroll JS -->
+    <script src="{{ asset('assets/js/jquery.slimscroll.min.js') }}"></script>
+    <!-- Select2 JS -->
+    <script src="{{ asset('assets/js/select2.min.js') }}"></script>
+    <!-- Datetimepicker JS -->
+    <script src="{{ asset('assets/js/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+    <!-- Custom JS -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+    @yield('script')
+</body>
 </html>
