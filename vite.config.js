76f9ecd2dfa1be03aedabf11533344ecbce93a24
Syaichul <syaichulahmad@gmail.com>
2024-08-19 15:57:53 +0700

    vercel v10

--- vite.config.js
+++ vite.config.js
@@ -11,20 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '@': 'resources/js',
-            '@sass': 'resources/sass',
-        },
-    },
     build: {
-        outDir: 'public/build/assets',
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                chunkFileNames: 'js/[name]-[hash].js',
-                entryFileNames: 'js/[name]-[hash].js',
-            },
-        },
+        outDir: 'public/build',
     },
 });
