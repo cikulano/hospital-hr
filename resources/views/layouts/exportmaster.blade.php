c53abb7d021cf427d1a6c5e97083b8edabb44f5b
Syaichul <syaichulahmad@gmail.com>
2024-08-20 14:35:43 +0700

    vercel v17

--- resources/views/layouts/exportmaster.blade.php
+++ resources/views/layouts/exportmaster.blade.php
@@ -12,24 +12,24 @@
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('assets/img/favicon.png') }}">
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
 	<!-- Fontawesome CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/font-awesome.min.css') }}">
 	<!-- Lineawesome CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/line-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/line-awesome.min.css') }}">
 	<!-- Datatable CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/dataTables.bootstrap4.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/dataTables.bootstrap4.min.css') }}">
 	<!-- Select2 CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/select2.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/select2.min.css') }}">
 	<!-- Datetimepicker CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap-datetimepicker.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap-datetimepicker.min.css') }}">
 	<!-- Chart CSS -->
-	<link rel="stylesheet" href="{{ URL::to('ssets/plugins/morris/morris.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('ssets/plugins/morris/morris.css') }}">
 	<!-- Main CSS -->
-	<link rel="stylesheet" href="{{ URL::to('assets/css/style.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 
 	{{-- message toastr --}}
-	<link rel="stylesheet" href="{{ URL::to('assets/css/toastr.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('assets/css/toastr.min.css') }}">
 	<script src="{{ URL::to('assets/js/toastr_jquery.min.js') }}"></script>
 	<script src="{{ URL::to('assets/js/toastr.min.js') }}"></script>
 
--- resources/views/layouts/settings.blade.php
+++ resources/views/layouts/settings.blade.php
@@ -11,17 +11,17 @@
 		<!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('assets/img/favicon.png') }}">
 		<!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
 		<!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{ URL::to('assets/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/font-awesome.min.css') }}">
 		<!-- Lineawesome CSS -->
-        <link rel="stylesheet" href="{{ URL::to('assets/css/line-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/line-awesome.min.css') }}">
 		<!-- Select2 CSS -->
-		<link rel="stylesheet" href="{{ URL::to('assets/css/select2.min.css') }}">
+		<link rel="stylesheet" href="{{ secure_asset('assets/css/select2.min.css') }}">
 		<!-- Main CSS -->
-        <link rel="stylesheet" href="{{ URL::to('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 		{{-- message toastr --}}
-		<link rel="stylesheet" href="{{ URL::to('assets/css/toastr.min.css') }}">
+		<link rel="stylesheet" href="{{secure_asset('assets/css/toastr.min.css') }}">
 		<script src="{{ URL::to('assets/js/toastr_jquery.min.js') }}"></script>
 		<script src="{{ URL::to('assets/js/toastr.min.js') }}"></script>
 
