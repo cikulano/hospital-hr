a27dcfbfc11664e4f73e674a44c44c982ebfd2a2
Syaichul <syaichulahmad@gmail.com>
2024-08-20 14:14:55 +0700

    vercel v21

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,26 +12,26 @@
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('assets/img/favicon.png') }}">
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.min.css') }}">
 	<!-- Fontawesome CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/font-awesome.min.css') }}">
 	<!-- Lineawesome CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/line-awesome.min.css') }}">
 	<!-- Datatable CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap4.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/dataTables.bootstrap4.min.css') }}">
 	<!-- Select2 CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/select2.min.css') }}">
 	<!-- Datetimepicker CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap-datetimepicker.min.css') }}">
 	<!-- Chart CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/plugins/morris/morris.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('ssets/plugins/morris/morris.css') }}">
 	<!-- Main CSS -->
-	<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('assets/css/style.css') }}">
 
 	{{-- message toastr --}}
 	<link rel="stylesheet" href="{{ URL::to('assets/css/toastr.min.css') }}">
-	<script src="{{ asset('assets/js/toastr_jquery.min.js') }}"></script>
-	<script src="{{ asset('assets/js/toastr.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/toastr_jquery.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/toastr.min.js') }}"></script>
 </head>
 
 <body>
@@ -157,4 +157,215 @@
 												<img alt="" src="{{ URL::to('/assets/images/'.Auth::user()->avatar) }}">
 											</span>
 											<div class="media-body">
-												<p class="noti-details"><span class="noti-title">Misty Tison</span
+												<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
+												<p class="noti-time"><span class="notification-time">8 mins ago</span></p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="activities.html">
+										<div class="media">
+											<span class="avatar">
+												<img alt="" src="{{ URL::to('/assets/images/'.Auth::user()->avatar) }}">
+											</span>
+											<div class="media-body">
+												<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
+												<p class="noti-time"><span class="notification-time">12 mins ago</span></p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="activities.html">
+										<div class="media">
+											<span class="avatar">
+												<img alt="" src="{{ URL::to('/assets/images/'.Auth::user()->avatar) }}">
+											</span>
+											<div class="media-body">
+												<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
+												<p class="noti-time"><span class="notification-time">2 days ago</span></p>
+											</div>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="topnav-dropdown-footer"> <a href="activities.html">View all Notifications</a> </div>
+					</div>
+				</li>
+				<!-- /Notifications -->
+				
+				<!-- Message Notifications -->
+				<li class="nav-item dropdown">
+					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+						<i class="fa fa-comment-o"></i> <span class="badge badge-pill">8</span>
+					</a>
+					<div class="dropdown-menu notifications">
+						<div class="topnav-dropdown-header">
+							<span class="notification-title">Messages</span> 
+							<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+						 </div>
+						<div class="noti-content">
+							<ul class="notification-list">
+								<li class="notification-message">
+									<a href="chat.html">
+										<div class="list-item">
+											<div class="list-left">
+												<span class="avatar">
+													<img alt="" src="{{ URL::to('/assets/images/'.Auth::user()->avatar) }}">
+												</span>
+											</div>
+											<div class="list-body">
+												<span class="message-author">Richard Miles </span> 
+												<span class="message-time">12:28 AM</span>
+												<div class="clearfix"></div>
+												<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span> 
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="chat.html">
+										<div class="list-item">
+											<div class="list-left">
+												<span class="avatar">
+													<img alt="" src="{{ URL::to('/assets/images/'. Auth::user()->avatar) }}">
+												</span>
+											</div>
+											<div class="list-body">
+												<span class="message-author">John Doe</span> 
+												<span class="message-time">6 Mar</span>
+												<div class="clearfix"></div> 
+												<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span> 
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="chat.html">
+										<div class="list-item">
+											<div class="list-left">
+												<span class="avatar">
+													<img alt="" src="{{ URL::to('/assets/images/'. Auth::user()->avatar) }}">
+												</span>
+											</div>
+											<div class="list-body">
+												<span class="message-author"> Tarah Shropshire </span>
+												<span class="message-time">5 Mar</span>
+												<div class="clearfix"></div> 
+												<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span> 
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="chat.html">
+										<div class="list-item">
+											<div class="list-left">
+												<span class="avatar">
+													<img alt="" src="{{ URL::to('/assets/images/'. Auth::user()->avatar) }}">
+													</span>
+												</div>
+											<div class="list-body">
+												<span class="message-author">Mike Litorus</span>
+												<span class="message-time">3 Mar</span>
+												<div class="clearfix"></div>
+												<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span> 
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="chat.html">
+										<div class="list-item">
+											<div class="list-left">
+												<span class="avatar">
+													<img alt="" src="{{ URL::to('/assets/images/'.Auth::user()->avatar) }}">
+												</span>
+											</div>
+											<div class="list-body">
+												<span class="message-author"> Catherine Manseau </span>
+												<span class="message-time">27 Feb</span>
+												<div class="clearfix"></div>
+												<span class="message-content">Lorem ipsum dolor sit amet, consectetur adipiscing</span>
+											</div>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="topnav-dropdown-footer"> <a href="chat.html">View all Messages</a> </div>
+					</div>
+				</li>
+				<!-- /Message Notifications -->
+				<li class="nav-item dropdown has-arrow main-drop">
+					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+						<span class="user-img">
+						<img src="{{ URL::to('/assets/images/'. Auth::user()->avatar) }}" alt="{{ Auth::user()->name }}">
+						<span class="status online"></span></span>
+						<span>{{ Session::get('name') }}</span>
+					</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="{{ route('profile_user') }}">My Profile</a>
+						<a class="dropdown-item" href="{{ route('company/settings/page') }}">Settings</a>
+						<a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+					</div>
+				</li>
+			</ul>
+			<!-- /Header Menu -->
+
+			<!-- Mobile Menu -->
+			<div class="dropdown mobile-user-menu">
+				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					<i class="fa fa-ellipsis-v"></i>
+				</a>
+				<div class="dropdown-menu dropdown-menu-right">
+					<a class="dropdown-item" href="{{ route('profile_user') }}">My Profile</a>
+					<a class="dropdown-item" href="{{ route('company/settings/page') }}">Settings</a>
+					<a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+				</div>
+			</div>
+			<!-- /Mobile Menu -->
+
+		</div>
+		<!-- /Header -->
+		<!-- Sidebar -->
+		@include('sidebar.sidebar')
+		<!-- /Sidebar -->
+		<!-- Page Wrapper -->
+		@yield('content')
+		<!-- /Page Wrapper -->
+	</div>
+	<!-- /Main Wrapper -->
+
+	<!-- jQuery -->
+	<script src="{{ URL::to('assets/js/jquery-3.5.1.min.js') }}"></script>
+	<!-- Bootstrap Core JS -->
+	<script src="{{ URL::to('assets/js/popper.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
+	<!-- Chart JS -->
+	<script src="{{ URL::to('assets/plugins/morris/morris.min.js') }}"></script>
+	<script src="{{ URL::to('assets/plugins/raphael/raphael.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/chart.js') }}"></script>
+	<script src="{{ URL::to('assets/js/Chart.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/line-chart.js') }}"></script>	
+	<!-- Slimscroll JS -->
+	<script src="{{ URL::to('assets/js/jquery.slimscroll.min.js') }}"></script>
+	<!-- Select2 JS -->
+	<script src="{{ URL::to('assets/js/select2.min.js') }}"></script>
+	<!-- Datetimepicker JS -->
+	<script src="{{ URL::to('assets/js/moment.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+	<!-- Datatable JS -->
+	<script src="{{ URL::to('assets/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/dataTables.bootstrap4.min.js') }}"></script>
+	<!-- Multiselect JS -->
+	<script src="{{ URL::to('assets/js/multiselect.min.js') }}"></script>
+	<!-- validation-->
+	<script src="{{ URL::to('assets/js/jquery.validate.js') }}"></script>	
+	<!-- Custom JS -->
+	<script src="{{ URL::to('assets/js/app.js') }}"></script>
+	@yield('script')
+</body>
+</html>
