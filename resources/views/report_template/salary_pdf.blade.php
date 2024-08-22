c150dc28538f0a120b70d472c16c5b582213c260
Syaichul <syaichulahmad@gmail.com>
2024-08-22 11:21:21 +0700

    back boys

--- resources/views/report_template/salary_pdf.blade.php
+++ resources/views/report_template/salary_pdf.blade.php
@@ -89,7 +89,7 @@
         <table width="100%">
             <tr>
                 <td width="30%" style="vertical-align: middle;">
-                    <img src="{{ public_path('assets/img/logo2.png') }}"alt="Company Logo" class="logo">
+                    <img src="D:\Sy\hr_ms_laravel_11\public\img\logo.png"alt="Company Logo" class="logo">
                 </td>
                 <td width="80%" style="vertical-align: middle;">
                     <div class="company-info">
@@ -184,6 +184,7 @@
                 <td class="salary-info-value">Rp {{ number_format($JHT) }}</td>
             </tr>
             <tr>
+            <tr>
                 <td class="salary-info-label">Iuran JP 1%</td>
                 <td class="salary-info-value">Rp {{ number_format($JP) }}</td>
             </tr>
@@ -211,7 +212,7 @@
     </div>
 
     <div class="watermark">
-        <img src="D:\Sy\hr_ms_laravel_11\public\assets\img\logo.png" alt="Watermark">
+        <img src="D:\Sy\hr_ms_laravel_11\public\img\logo.png" alt="Watermark">
     </div>
 </body>
 </html>
