d8e8b33706a31bd9f3c5b8ad00b3bbddca7f1d81
Syaichul <syaichulahmad@gmail.com>
2024-08-22 08:06:23 +0700

    logo pdf v1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '32c091ce6b1473b4a39a2f0aa7fffa796f7d977a',
+        'reference' => '9339960a23f959d455ad3879ee1a110d930d1eab',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -409,6 +409,24 @@
                 0 => 'v11.10.0',
             ),
         ),
+        'intervention/gif' => array(
+            'pretty_version' => '4.1.0',
+            'version' => '4.1.0.0',
+            'reference' => '3a2b5f8a8856e8877cdab5c47e51aab2d4cb23a3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../intervention/gif',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'intervention/image' => array(
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'reference' => '1786ad5e1789050939d73cd195de4b8eaeeb34ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../intervention/image',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'kodova/hamcrest-php' => array(
             'dev_requirement' => true,
             'replaced' => array(
@@ -436,7 +454,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '32c091ce6b1473b4a39a2f0aa7fffa796f7d977a',
+            'reference' => '9339960a23f959d455ad3879ee1a110d930d1eab',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
